e5fc30ef0305be6bbc9e861f9d8b9510d2b60dca
PabloJavier22 <pmungar408@g.educaand.es>
2024-02-02 14:26:55 +0100

    creación del metodo post

--- apiGumcraft/Controllers/GumcraftController.cs
+++ apiGumcraft/Controllers/GumcraftController.cs
@@ -18,10 +18,23 @@ namespace apiGumcraft.Controllers
 
         //Devuelve user
 
-        [HttpGet]
+        [HttpGet("getusers")]
         public IEnumerable<User> GetUsers()
         {
             return _dbContext.Users;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Users.Add(user);
+                await _dbContext.SaveChangesAsync();
+                return Ok("User created successfully");//no se si dejarlo en ingles
+            }
+
+            return BadRequest("Error");
         }
     }
 }
