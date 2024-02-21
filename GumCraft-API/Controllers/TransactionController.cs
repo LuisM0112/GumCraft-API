a3aed564accc7359937eaffeefd823a6160737b6
LuisM0112 <149406770+LuisM0112@users.noreply.github.com>
2024-02-21 18:09:34 +0100

    creación del controlador

---
+++ GumCraft-API/Controllers/TransactionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GumCraft_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+    }
+}
