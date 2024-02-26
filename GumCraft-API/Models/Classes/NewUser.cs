7fb1da1025f4a8d41c751eef6e3957b5d1ebd132
LuisM0112 <luismigase@gmail.com>
2024-02-26 18:02:05 +0100

    Corrección

--- GumCraft-API/Models/Classes/NewUser.cs
+++ GumCraft-API/Models/Classes/NewUser.cs
@@ -2,10 +2,10 @@
 {
     public class NewUser
     {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string PasswordBis { get; set; }
-        public string Address { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? PasswordBis { get; set; }
+        public string? Address { get; set; }
     }
 }
