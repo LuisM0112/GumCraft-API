6a4ea3011d06e19467b78bfdf2a76744f1c9823e
LuisM0112 <luismigase@gmail.com>
2024-03-03 21:03:32 +0100

    Imagenes y productos

--- GumCraft-API/Models/Database/DbSeeder.cs
+++ GumCraft-API/Models/Database/DbSeeder.cs
@@ -25,16 +25,104 @@ public class DbSeeder
     private async Task SeedProductsAsync()
     {
         Product[] products =
-        [
-            new Product()
-            {
-                Name = "Bote chicles redstone",
-                Description = "Chicles de fresa con relleno ácido",
-                Image = "images/redstone.png",
-                Stock = 300,
-                EURprice = 3.99m
-            }
-        ];
+         [
+             new Product()
+             {
+                 Name = "Bote chicles redstone",
+                 Description = "Chicles de fresa con relleno ácido",
+                 Image = "images/redstone.png",
+                 Stock = 300,
+                 EURprice = 15.59m
+             },
+             new Product()
+             {
+                 Name = "Bote chicles hierro",
+                 Description = "Chicles de limón con relleno ácido",
+                 Image = "images/hierro.png",
+                 Stock = 300,
+                 EURprice = 15.59m
+             },
+             new Product()
+             {
+                 Name = "Bote chicles diamante",
+                 Description = "Chicles de menta con relleno menta",
+                 Image = "images/diamante.png",
+                 Stock = 300,
+                 EURprice = 15.59m
+             },
+             new Product()
+             {
+                 Name = "Bote chicles esmeralda",
+                 Description = "Chicles de sabor hierbabuena",
+                 Image = "images/esmeralda.png",
+                 Stock = 300,
+                 EURprice = 12.59m
+             },
+             new Product()
+             {
+                 Name = "Bote chicles lapislazuli",
+                 Description = "Chicles de sabor arándanos",
+                 Image = "images/lapislazuli.png",
+                 Stock = 300,
+                 EURprice = 12.59m
+             },
+             new Product()
+             {
+                 Name = "Bote chicles obsidiana",
+                 Description = "Chicles de sabor regaliz",
+                 Image = "images/obsidiana.png",
+                 Stock = 300,
+                 EURprice = 12.59m
+             },
+             new Product()
+             {
+                 Name = "200 unidades chicles redstone",
+                 Description = "Chicles de fresa con relleno ácido",
+                 Image = "images/redstone.png",
+                 Stock = 300,
+                 EURprice = 11m
+             },
+             new Product()
+             {
+                 Name = "200 unidades chicles hierro",
+                 Description = "Chicles de limón con relleno ácido",
+                 Image = "images/hierro.png",
+                 Stock = 300,
+                 EURprice = 11m
+             },
+             new Product()
+             {
+                 Name = "200 unidades chicles diamante",
+                 Description = "Chicles de menta con relleno menta",
+                 Image = "images/diamante.png",
+                 Stock = 300,
+                 EURprice = 11m
+             },
+             new Product()
+             {
+                 Name = "200 unidades chicles esmeralda",
+                 Description = "Chicles de sabor hierbabuena",
+                 Image = "images/esmeralda.png",
+                 Stock = 300,
+                 EURprice = 10m
+             },
+             new Product()
+             {
+                 Name = "200 unidades chicles lapislazuli",
+                 Description = "Chicles de sabor arándanos",
+                 Image = "images/lapislazuli.png",
+                 Stock = 300,
+                 EURprice = 10m
+             },
+             new Product()
+             {
+                 Name = "200 unidades chicles obsidiana",
+                 Description = "Chicles de sabor regaliz",
+                 Image = "images/obsidiana.png",
+                 Stock = 300,
+                 EURprice = 10m
+             }
+         ];
         await _dbcontext.Products.AddRangeAsync(products);
     }
 }
